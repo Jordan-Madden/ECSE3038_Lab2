6229a4af509857c16548b512acf7c2a2de3a5b50
Jordan-Madden <jordanmadden285@gmail.com>
2021-02-04 16:00:46 -0500

    Essentially finished

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 '''
-    ECSE3038 Tutorial 1
-    Description: Build a basic HTTP REST API with Flask
+    Author: Jordan Madden
+    Description: ECSE3038 Lab 2
 '''
 
 from flask import Flask, request, jsonify
+from datetime import datetime
 from copy import deepcopy
 import json
 
 app = Flask(__name__)
 
 # Super fancy database
-SUPER_DB = [
+PROFILE_DB = [
     {
         "success": True,
         "data": {
@@ -22,57 +23,85 @@ SUPER_DB = [
     }
 ]
 
+TANK_DB = []
+
 # Returns all of the data in the database
 @app.route("/profile", methods=["GET"])
 def get_profile():
-    return jsonify(SUPER_DB)
+    return jsonify(PROFILE_DB)
 
 # Returns whatever object it recieves in the body of the request
 @app.route("/profile", methods=["POST"])
 def post_profile():
-    copy_db = deepcopy(SUPER_DB)
-    c = copy_db[0]
-    c["data"] = request.json
-    SUPER_DB.append(c)
+    # Get the current date and time
+    now = datetime.now()
+    dt = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    r = request.json
+    r["last_updated"] = dt
+    PROFILE_DB[0]["data"] = r
+
     return request.json
 
 
 # Returns 
 @app.route('/profile', methods=["PATCH"])
-def patch_profile(id):
-    for i in SUPER_DB:
-        if i["id"] == id:
-            i["name"] = request.json
+def patch_profile():
+    # Get the current date and time
+    now = datetime.now()
+    dt = now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    data = PROFILE_DB[0]["data"]
+
+    r = request.json
+    r["last_updated"] = dt
+    attributes = r.keys()
+    for attribute in attributes:
+        data[attribute] = r[attribute]
+
     return request.json
+
 
 ###############################################################################
 
-# Returns all of the data in SUPER_DB
+# Returns all of the data in TANK_DB
 @app.route("/data", methods=["GET"])
 def get_data():
-    return jsonify(SUPER_DB)
+    return jsonify(TANK_DB)
 
 # Returns whatever object it recieves in the body of the request
 @app.route("/data", methods=["POST"])
 def post_data():
-    SUPER_DB.append(request.json)
+    id = len(TANK_DB) + 1
+    
+    r = request.json
+    r["id"] = id
+    TANK_DB.append(r)
     return request.json
 
 # Returns 
 @app.route('/data/<int:id>', methods=["PATCH"])
 def patch_data(id):
-    for i in SUPER_DB:
-        if i["data"] == id:
-            i["name"] = request.json
+    for i in TANK_DB:
+        if i["id"] == id:
+            r = request.json
+            attributes = r.keys()
+
+            for attribute in attributes:
+                i[attribute] = r[attribute]
+
     return request.json
 
 # Returns 
 @app.route("/data/<int:id>", methods=["DELETE"])
 def delete_data(id):
-    for i in SUPER_DB:
+    for i in TANK_DB:
         if i["id"] == id:
-            SUPER_DB.remove(i)
-    return request.json
+            TANK_DB.remove(i)
+
+    return {
+        "success": True
+        }
 
 
 if __name__ == "__main__":
